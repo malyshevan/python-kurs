76dc6ec95c6e4a779ff8685f53c886bb0f1c8367
malyshevan <Aleksey.90@inbox.ru>
2017-05-21 17:06:22 +0300

    Add files via upload

---
+++ domashka-1.py
@@ -0,0 +1,23 @@
+# Задание 1
+#a = 10 * 100
+#b = 15 * 25
+#c = a - b
+#print (c)
+
+# Задание 2
+#plates = int(input('Количество тарелок: '))
+#detergent = int(input('Количество моющего средства: '))
+#detergent1 = plates * 0.5
+#print ('Количество требуемового моющего средства:', detergent1)
+#while (plates <= detergent1):	
+ #   detergent1 = plates * 0.5
+ #   plates1 = (detergent * 2) - plates 
+  #  print ('Количество немытых тарелок:', plates1)  
+  #  break
+  #  plates += 1  
+#else:
+	#detergent2 = detergent - detergent1
+	#print ('Количество оставшегося моющего средства:', detergent2)
+
+# Задание 3
+
