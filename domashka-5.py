afe46c37f6a024425a9b01183df139331105f3d9
malyshevan <Aleksey.90@inbox.ru>
2017-06-28 13:13:48 +0300

    Update domashka-5.py

--- domashka-5.py
+++ domashka-5.py
@@ -19,9 +19,30 @@ for n in fibonacci(10):
 #def ran(n):
 #    yield ''.join([ascii_uppercase[random.randrange(len(ascii_uppercase))] for i in range(n)])
 
-#for i in ran(10):
-#    print(i)
+#gen = ran(int(input('Введите длину пароля: ')))
+#print('пароль: {}'.format(next(gen)))
 
+
+#Задача3
+import random
+import time
+from time import sleep
+from string import ascii_uppercase
+
+
+def decorator(pausa):
+    for x in range(1):
+        time.sleep(3)
+        print("Ваша операция выполняется...")
+    return decorator
+
+@decorator
+
+def ran(n):
+    yield ''.join([ascii_uppercase[random.randrange(len(ascii_uppercase))] for i in range(n)])
+
+gen = ran(int(input('Укажите длину пароля: ')))
+print('Пароль: {} '.format(next(gen)))
 
 
 
